a553d201063d0e4c2b27bb1b9263f6b3d4d4909e
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-15 07:33:36 +0000

    make dev branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "cds#index"
+  root 'cds#index'
+
   get '/thanks', to: 'thanks#thanks'
 
   devise_for :users
