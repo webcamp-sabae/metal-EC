ae55fbfc0a82e7bc500a7f44fcf4d5ed86d9b932
su-san1129 <deviationvalue1129@icloud.com>
2019-05-16 05:15:57 +0000

    [change]add_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_094846) do
+ActiveRecord::Schema.define(version: 2019_05_16_045012) do
 
   create_table "artists", force: :cascade do |t|
     t.string "artist_name", null: false
@@ -90,6 +90,13 @@ ActiveRecord::Schema.define(version: 2019_05_15_094846) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "familyname"
+    t.string "firstname"
+    t.string "kana_familyname"
+    t.string "kana_firstname"
+    t.string "postal_code"
+    t.string "telephone_number"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
