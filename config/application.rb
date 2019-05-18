8eb06163824655a24718f754e51b791ca5d13302
yujiloh0816 <48126468+yujiloh0816@users.noreply.github.com>
2019-05-18 14:47:57 +0900

    Remove config

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,5 @@ module MetalEc
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
-    config.generators do |g|
-      g.skip_routes true
-    end
-
-    
   end
 end
