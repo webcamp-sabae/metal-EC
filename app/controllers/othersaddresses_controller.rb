a6c3674e3e1cbe5c041435058352c1d4756a2ef3
osuke-noguchi <noguchi.nodai@gmail.com>
2019-05-20 07:22:04 +0000

    add othersaddress view

--- app/controllers/othersaddresses_controller.rb
+++ app/controllers/othersaddresses_controller.rb
@@ -8,6 +8,7 @@ class OthersaddressesController < ApplicationController
 
   def create
     @othersaddress = Othersaddress.new(othersaddress_params)
+    @othersaddress.user_id = 1
     if @othersaddress.save
     redirecit_to new_receipt_path
     else
@@ -23,6 +24,6 @@ class OthersaddressesController < ApplicationController
   private
 
   def othersaddress_params
-    params.require(:othersaddress).permit(:user_id,:familyname, :firstname, :kana_familyname, :kana_firstname, :telephone_number, :postal_code, :address)
+    params.require(:othersaddress).permit(:user_id,:familyname, :firstname, :kana_familyname, :kana_firstname, :first_postal_code, :last_postal_code, :statu_address, :city_address, :street_address, :telephone_number1, :telephone_number2, :telephone_number3)
   end
 end
