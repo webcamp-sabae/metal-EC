c395392c789618d54ca462173755303fc8a4ea2e
su-san1129 <deviationvalue1129@icloud.com>
2019-05-20 11:54:18 +0000

    cds layout change

--- app/controllers/admins/artists_controller.rb
+++ app/controllers/admins/artists_controller.rb
@@ -1,13 +1,16 @@
 class Admins::ArtistsController < Admins::AdminsController
 	def new
 		@artist = Artist.new
-		@artists = Artist.all
 	end
 
 	def create
 		@artist = Artist.new(artist_params)
 		@artist.save
 		redirect_to new_admins_cd_path
+	end
+
+	def index
+		@artists = Artist.all
 	end
 
 	def edit
@@ -17,6 +20,9 @@ class Admins::ArtistsController < Admins::AdminsController
 	end
 
 	def destroy
+		@artist = Artist.find(params[:id])
+		@artist.destroy
+		redirect_to new_admins_cd_path
 	end
 
 	private
