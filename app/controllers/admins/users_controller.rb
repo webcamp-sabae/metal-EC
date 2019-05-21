9d1c1415d8e829c697e92283e32b84a9133a1ebc
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-21 07:45:28 +0000

    Add set-user on admins/usercontroller

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,7 +1,5 @@
-# class Admins::UsersController < ApplicationController
-# layout "admins"
-# AdminsControllerを継承するように変更
 class Admins::UsersController < Admins::AdminsController
+  before_action :set_user, only: [:show, :edit]
 
   def index
     @users = User.all
@@ -12,4 +10,20 @@ class Admins::UsersController < Admins::AdminsController
 
   def edit
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 end
+
+
+# <%= form.text_field :email %>
+# <%= form.submit %>
+#
+# <%= form_with(...) do |form| %>
+#   <%= form.label(:genre) %>
+#   <%= form.text_field(:genre, id: :title_genre) %>
+# <% end %>
