78c95c949677963b609f211e865e308f4d1ab543
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-22 10:18:51 +0000

    Add update action to user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update]
 
   def show
   	@user = User.find(params[:id])
@@ -6,6 +7,7 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
+
   	if @user.save
   		redirect_to user_path(current_user.id)
   	else
@@ -18,14 +20,40 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
   def destory
 
   end
   private
 
+    def set_user
+      @user = User.find(params[:id])
+    end
+
   def user_params
-  	params.require(:user).pamit(:familyname, :firstname, :kana_familyname, :kana_firstname, :first_postal_code, :last_postal_code, :statu_address, :city_address, :street_address, :telephone_number1, :telephone_number2, :telephone_number3)
+  	params.require(:user).permit(
+      :familyname,
+      :firstname,
+      :kana_familyname,
+      :kana_firstname,
+      :first_postal_code,
+      :last_postal_code,
+      :statu_address,
+      :city_address,
+      :street_address,
+      :telephone_number1,
+      :telephone_number2,
+      :telephone_number3
+    )
   end
 end
