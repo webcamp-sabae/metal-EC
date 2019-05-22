c27126862c17886b22dd0e5c5e7a160751f16aa9
su-san1129 <deviationvalue1129@icloud.com>
2019-05-22 10:39:32 +0000

    layout change

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -3,8 +3,8 @@ class Cd < ApplicationRecord
 	belongs_to :artist
 	belongs_to :genre
 	belongs_to :label
-	has_many :songs
-	accepts_nested_attributes_for :songs, allow_destroy: true
+	has_many :songs, inverse_of: :cd
+  accepts_nested_attributes_for :songs, reject_if: :all_blank, allow_destroy: true
 
 	attachment :cd_image
 end
