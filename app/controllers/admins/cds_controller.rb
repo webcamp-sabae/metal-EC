8350d425ef7c2c0801473d4744359178b5da8b19
su-san1129 <deviationvalue1129@icloud.com>
2019-05-23 03:48:48 +0000

    change layout

--- app/controllers/admins/cds_controller.rb
+++ app/controllers/admins/cds_controller.rb
@@ -1,5 +1,7 @@
 class Admins::CdsController < Admins::AdminsController
 
+	before_action :set_cds, only: [:show, :edit, :update, :destroy]
+
 	def new
 		@cd = Cd.new
 		@artist = Artist.new
@@ -12,34 +14,42 @@ class Admins::CdsController < Admins::AdminsController
 	end
 
 	def show
-		@cd = Cd.find(params[:id])
+
 	end
 
 	def edit
-		@cd = Cd.find(params[:id])
 	end
 
 
 	def create
 		@cd = Cd.new(cd_params)
 		@cd.save
-		logger.debug @cd.errors.to_yaml
 		redirect_to new_admins_cd_path
 	end
 
 	def update
-  	@cd = Cd.find(params[:id])
   	if @cd.update(cd_params)
   	   redirect_to admins_cd_path(@cd.id), notice: "cd was successfully updated."
     else render :edit
-  end
+   end
+	end
+
+	def destroy
+  	@cd.destroy
+  	redirect_to admins_cds_path, notice: "cd was successfully destroyed"
 	end
 
 
 	private
+
+		def set_cds
+			@cd = Cd.find(params[:id])
+		end
+
 		def cd_params
 			params.require(:cd).permit(:artist_id, :label_id, :genre_id, :release,
-			 :price, :stock, :single_album_name, :cd_image, songs_attributes: [:disc_num, :song_title])
+			 :price, :stock, :single_album_name, :cd_image,
+			 songs_attributes: [:id, :disc_num, :song_title, :_destroy])
 		end
 
 end
