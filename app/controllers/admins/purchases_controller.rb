ed4d0b626616eeb5a768cb019dede95f33d92ea5
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-25 05:04:26 +0000

    Add ransack gem

--- app/controllers/admins/purchases_controller.rb
+++ app/controllers/admins/purchases_controller.rb
@@ -1,5 +1,24 @@
 class Admins::PurchasesController < Admins::AdminsController
   def index
-    @purchases = Purchase.all
+    @q = Purchase.ransack(params[:q])
+    @purchases = @q.result
+
+    # @term = Purchase.ransack(params[:q])
+    # @purchases = @term.result
+    # @purchases = Purchase.all
   end
+
 end
+
+# def index
+#   unless params[:q].present?
+#     @q = User.ransack
+#   else
+#     @q = User.ransack(
+#       name_or_nationality_cont_any:
+#       params[:q][:name_or_nationality_cont_any].split(/[\s|　]/)
+#     )
+#   end
+#  @users = @q.result
+#  @q_sql = @q.result.to_sql
+# end
