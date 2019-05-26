f5788f7c6f79f2573a629dabac2fcbace2b1032a
su-san1129 <deviationvalue1129@icloud.com>
2019-05-26 06:00:13 +0000

    change javascript file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,28 +24,3 @@
 //= require jquery_ujs
 //= require cocoon
 
-<script>
-  $(function(){
-    $fileField = $('#file')
-
-    // 選択された画像を取得し表示
-    $($fileField).on('change', $fileField, function(e) {
-      file = e.target.files[0]
-      reader = new FileReader(),
-      $preview = $("#img_field");
-
-      reader.onload = (function(file) {
-        return function(e) {
-          $preview.empty();
-          $preview.append($('<img>').attr({
-            src: e.target.result,
-            width: "50%",
-            class: "preview",
-            title: file.name
-          }));
-        };
-      })(file);
-      reader.readAsDataURL(file);
-    });
-  });
-</script>
