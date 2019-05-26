ab219b12704c65c6131df6c2561ba85431b0ecf4
mogumogura <ankokagura@gmail.com>
2019-05-26 08:11:18 +0000

    update cds/show

--- app/controllers/cds_controller.rb
+++ app/controllers/cds_controller.rb
@@ -2,6 +2,15 @@ class CdsController < ApplicationController
   def show
   	@cd = Cd.find(params[:id])
   	@song = Song.find(params[:id])
+
+  	@stock_array = []
+  	@cd.stock.times do |quantity|
+  		if quantity < 15
+  			 @stock_array << quantity + 1
+  			else
+  				break
+  			end
+  		end
   end
 
   def index
