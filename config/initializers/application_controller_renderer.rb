63e95caf0394a1b89b7f679bb89516ab5f19376d
mogumogura <ankokagura@gmail.com>
2019-05-28 05:59:28 +0000

    update cd/index, user/show Views

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4b38bb5158e94deac5ab3b27c76ec6c81a5c18b086dacc79bf07e044a4214fdf444f7ff006382a41fae4ed86e43b8ccdc9be4eada5d4ef722e01d850aa9156c9'
