3352013ac6876b8e56945c8105e02f6adb5120d5
osuke-noguchi <noguchi.nodai@gmail.com>
2019-05-29 03:21:20 +0000

    fix-receipt

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -6,7 +6,7 @@ class Receipt < ApplicationRecord
   enum payment: { 銀行振込:1, クレジットカード:2, 代金引換:3 }
   enum status: { 未発送: 1, 発送済: 2 }
 
-  # validates :ship, :shipping_address, presence: true
+
 
 # クラスメソッド status情報のみ取得
   def self.status_select(status)
